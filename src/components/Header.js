e2220900ccd38d13899169a3154b0579cd0a0f84
Student <example@example.com>
2020-03-09 10:34:35 -0700

    get rid of white space at the top of the page and reposition header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 export default function Header() {
+    const headerPosition = {
+        position: 'fixed'
+    }
     return (
-        <div>
+        <div style={headerPosition}>
             <h1>The Good News API</h1>
         </div>
     )
