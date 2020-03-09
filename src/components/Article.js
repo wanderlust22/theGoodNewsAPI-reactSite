b155084becf5dd9c1e5911ed359f0fe9341f5687
Student <example@example.com>
2020-03-09 14:29:23 -0700

    create Article component and successful call API to create Article components based on the results of the call

---
+++ src/components/Article.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Article(props) {
+    return (
+        <div>
+            {console.log(props.title)}
+            <h3>{props.title}</h3>
+        </div>
+    )
+}
--- src/components/GETRequest.js
+++ src/components/GETRequest.js
@@ -1,21 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import Article from './Article';
 
 export default function GETRequest() {
 
     const [get, setGet] = useState(false);
+    const [random, setRandom] = useState([]);
 
     const getFrom = 'https://z756etr2ha.execute-api.us-east-2.amazonaws.com/production/item';
 
     const getRequest = async() => {
         const response = await fetch(getFrom);
         const data = await response.json();
-        console.log(data);
-        console.log(get);
+        setRandom(data);
+        setGet(false);
     }
 
     useEffect( () => {
-       getRequest();
-    }, [get === true])
+        if (get === true) {
+            getRequest();
+        }
+    }, [get])
 
     const GETStyle = {
         position: 'absolute',
@@ -25,6 +29,10 @@ export default function GETRequest() {
     return (
         <div style={GETStyle}>
             <button onClick={() => setGet(!get)} >GET request</button>
+            {console.log(random)}
+            { random.map(article => <Article key={article.id}
+            title={article.title}
+            />)}
         </div>
     )
 }
