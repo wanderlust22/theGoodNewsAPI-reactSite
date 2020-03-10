2a8b9b9dacf470e07ffbb99a61fd7b264d726c52
Student <example@example.com>
2020-03-10 16:06:02 -0700

    add basic form to UI for POST request

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Footer from './components/Footer';
 import GETRequest from './components/GETRequest';
+import POSTRequest from './components/POSTRequest';
 // https://medium.com/better-programming/how-to-build-a-serverless-api-with-dynamodb-aws-lambda-and-api-gateway-d61ac63c27dd
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <About />
       <GETRequest />
+      <POSTRequest />
       <Footer />
     </div>
   );
--- src/components/POSTRequest.js
+++ src/components/POSTRequest.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function POSTRequest() {
+
+    const [title, setTitle] = useState("");
+
+    const setTheTitle = (e) => {
+        let newTitle = e.target.value
+        setTitle(newTitle);
+    }
+
+    // const submitHandler = e => {
+    //     e.preventDefault();
+    //     console.log(title);
+    // }
+
     return (
         <div>
-            
+            <form>
+                <div>
+                    <input type="text" name="title" onChange={(e) => setTheTitle(e)}/>
+                </div>
+                <button type="submit">POST article</button>
+            </form>
         </div>
     )
 }
