07d1eabfa644cd7c8eab12da763e4be8121f6687
Student <example@example.com>
2020-03-10 10:32:24 -0700

    add About component and synch it with CSS grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';          
 import Header from './components/Header';
+import About from './components/About';
 import Footer from './components/Footer';
 import GETRequest from './components/GETRequest';
 // https://medium.com/better-programming/how-to-build-a-serverless-api-with-dynamodb-aws-lambda-and-api-gateway-d61ac63c27dd
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <About />
       <GETRequest />
       <Footer />
     </div>
---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function About() {
+
+    const aboutStyle = {
+        padding: '10px',
+        borderRadius: '10px',
+        boxShadow: '0px 5px 20px rgb(71, 71, 71)',
+        gridColumn: '1/4',
+        gridRow: '2/3'
+    }
+    return (
+        <div style={aboutStyle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, voluptates. Eaque enim eos inventore aliquid ea maxime, iure sit corrupti tempora atque saepe, dicta cupiditate architecto deserunt earum distinctio dolorum harum? Optio, nesciunt quae temporibus vel commodi assumenda molestias nisi aspernatur quisquam aperiam eius corrupti similique magni fugiat, quis placeat.
+        </div>
+    )
+}
