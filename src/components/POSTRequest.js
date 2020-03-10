eed9e7adabeeee5adcda51f62ae0d65a78270448
Student <example@example.com>
2020-03-10 16:15:45 -0700

    successful change state hook on submission of form

--- src/components/POSTRequest.js
+++ src/components/POSTRequest.js
@@ -9,16 +9,25 @@ export default function POSTRequest() {
         setTitle(newTitle);
     }
 
-    // const submitHandler = e => {
-    //     e.preventDefault();
-    //     console.log(title);
-    // }
+    const submitHandler = e => {
+        e.preventDefault();
+        console.log(title);
+    }
+
+    const POSTStyle = {
+        borderRadius: '10px',
+        boxShadow: '0px 5px 20px rgb(71, 71, 71)',
+        padding: '10px',
+        gridRow: '5/7',
+        gridColumn: '1/4',
+        margin: '5px'
+    }
 
     return (
-        <div>
-            <form>
+        <div style={POSTStyle}>
+            <form onSubmit={(e) => submitHandler(e)}>
                 <div>
-                    <input type="text" name="title" onChange={(e) => setTheTitle(e)}/>
+                    <input type="text" name="title" value={title} onChange={(e) => setTheTitle(e)}/>
                 </div>
                 <button type="submit">POST article</button>
             </form>
